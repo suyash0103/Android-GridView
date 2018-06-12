6d980449404fc88c5b432e680d8fc948f91f7bf8
suyash0103 <suyashghuge@gmail.com>
2018-06-13 00:44:08 +0530

    add gridview to main activity

--- app/src/main/java/com/example/android/gridview/MainActivity.java
+++ app/src/main/java/com/example/android/gridview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
     }
 }
